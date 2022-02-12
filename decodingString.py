b01808a6cf773170a9cfb61b1a59dd87a67383e4
Козлов Павел <pavel.kozlov@playrix.com>
2022-02-12 12:14:09 +0500

    легкий рефакторинг и добавил файлы

--- decodingString.py
+++ decodingString.py
@@ -1,4 +1,6 @@
-file = open('c:\\Denis.txt','r')
+fileNameRead = 'Denis.txt'
+fileNameWrite = 'Denis1.txt'
+file = open(fileNameRead,'r')
 string = file.readline().strip()
 file.close()
 newstring1 = ''
@@ -7,7 +9,7 @@ z = ''
 i = 0
 number = ''
 xi = 0
-file = open('file.txt', 'w')
+file = open(fileNameWrite, 'w')
 while i < len(string):
     y = string[i]
     if (y.isalpha()) == True:
@@ -21,9 +23,7 @@ while i < len(string):
                 number += y
                 if (xi + 1) < len(string):
                     xi += 1
-                    y = string[xi]
-                else:
-                    y = string[xi]
+                y = string[xi]
             number = int(number)
             file.write(newstring1 * number)
         if i == (len(string)-1):
